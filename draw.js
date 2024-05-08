31199964e6c9fdec73bcd7f9ba37183377c69d2b
Ale579 <alessandrotg9@gmail.com>
2024-05-08 16:44:37 +0200

    Make each square change color when mouse hovers over it

--- draw.js
+++ draw.js
@@ -1,6 +1,25 @@
 const container = document.querySelector("#container");
 
-let square = document.createElement("div");
-square.classList.add("square");
-container.appendChild(square);
+// Generate canvas
+for (let i = 0; i < 4; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let j = 0; j < 4; j++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        // set class to a position
+        square.classList.add(i+"."+j);
+
+        row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+
+// Change square color when mouse hovers over square
+container.addEventListener("mouseover", (event) => {
+    let target = event.target;
+
+    target.style.backgroundColor = "pink";
+});
 
