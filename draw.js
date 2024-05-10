975765f4a874a44f9c62e2780cd3559ca3146e70
Ale579 <alessandrotg9@gmail.com>
2024-05-10 16:12:27 +0200

    Decorate website and make generateCanvas function

--- draw.js
+++ draw.js
@@ -1,53 +1,47 @@
-let container = document.querySelector("#container");
-let size = 10;
+let canvas = document.querySelector("#container");
+let size = 16;
 
-let choseSize = document.querySelector("button");
+let choseSize = document.querySelector("#changeSize");
 
+// Change the size of the canvas
 choseSize.addEventListener("click", () => {
-    size = Math.floor(prompt("Number of squares per side: "));
+    do {
+        size = Math.floor(prompt("Number of squares per side: "));
+    } while (size > 100); 
 
-    document.getElementById("container").remove(); 
+    // Remove previous canvas
+    let oldCanvas = document.querySelectorAll(".row");
+    oldCanvas.forEach((row) => {
+        row.remove()
+    });
 
-    let container = document.createElement("div");
-    container.id = "container";
+    // Create new canvas
+    let canvas = document.createElement("div");
+    canvas.id = "container";
+    generateCanvas();
+});
 
-    // Generate canvas
+function generateCanvas () {
     for (let i = 0; i < size; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
-
+    
         for (let j = 0; j < size; j++) {
             let square = document.createElement("div");
             square.classList.add("square");
             row.appendChild(square);
             }
-            container.appendChild(row);
+        canvas.appendChild(row);  
     }
-    choseSize.parentNode.insertBefore(container, choseSize.nextSibling);
-
-    container.addEventListener("mouseover", (event) => {
-        let target = event.target;
-    
-        target.style.backgroundColor = "pink";
-    });
-});
-
-// Generate canvas
-for (let i = 0; i < size; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-
-    for (let j = 0; j < size; j++) {
-        let square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
-        }
-        container.appendChild(row);
+    choseSize.parentNode.insertBefore(canvas, choseSize.nextSibling)
 }
 
 // Change square color when mouse hovers over square
-container.addEventListener("mouseover", (event) => {
+canvas.addEventListener("mouseover", (event) => {
     let target = event.target;
 
     target.style.backgroundColor = "pink";
 });
+
+// generate canvas when the page is opened
+generateCanvas();
